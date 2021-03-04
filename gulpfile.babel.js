61e64ceeec696e71513221632a744cc2451d5591
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2021-03-04 18:27:38 +0500

    npm gulp installed

---
+++ gulpfile.babel.js
@@ -0,0 +1,17 @@
+
+import gulp from gulp
+
+// function defaultTask(cb) {
+//     // place code for your default task here
+//     cb();
+//   }
+
+export const hello = (done) => {
+  console.log ('hello');
+  done();
+}
+
+
+
+  
+  exports.default = defaultTask
